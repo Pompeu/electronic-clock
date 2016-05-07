62b129f9ba46b41eed99305fc65c8e49086e66e6
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2016-05-07 00:41:01 -0300

    Adds eslint to gulp tasks

---
+++ server/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp   = require('gulp');
+const gulpIf = require('gulp-if');
+const eslint = require('gulp-eslint');
+
+const sourceFiles = ['**/*.js', '!node_modules/**', '!coverage/**'];
+
+function isFixed (file) {
+    return file.eslint && typeof file.eslint.output === 'string';
+}
+
+gulp.task('eslint', () => {
+    return gulp.src(sourceFiles)
+        .pipe(eslint({
+            useEslintrc: true,
+            fix:         true
+        }))
+        .pipe(eslint.format())
+        .pipe(gulpIf(isFixed, gulp.dest('./')))
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(sourceFiles, ['eslint']);
+});
+
+gulp.task('default', ['watch']);
